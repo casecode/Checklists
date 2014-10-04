07cc0b720dc4ca32d005f9b5fa6f51e53663f02c
Casey White <casey.r.white@gmail.com>
2014-10-04 15:03:26 -0700

    02 - Arrays

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,18 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Casey R White on 10/4/14.
+//  Copyright (c) 2014 casecode. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+    
+    func toggleChecked() {
+        checked = !checked
+    }
+}
